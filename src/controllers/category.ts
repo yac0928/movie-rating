2e4a8857adc2f33d1dc58687797942ad11bbc780
yac0928 <kangarooinapark@gmail.com>
2024-05-11 15:48:22 +0800

    feat: add getCategory, putCategory, deleteCategory

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -1,27 +1,61 @@
-import Category from "../models/category"
+import Category from '../models/category'
 import { Context } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 export const categoryController = {
   getCategories: async (c: Context, next: (error?: any) => void) => {
     try {
-      const categories = await Category.find()
-      console.log('categories:', categories)
-      return c.json(categories)
+      throw new HTTPException(401, { message: 'Custom error message' })
+      // const categories = await Category.find()
+      // return c.json(categories)
     } catch (err) {
-      console.log(err)
+      next(err)
+    }
+  },
+  getCategory: async (c: Context, next: (error?: any) => void) => {
+    try {
+      const { cid } = c.req.param()
+      const category = await Category.findById(cid)
+      if (!category) throw new Error('Category not found!')
+      return c.json(category)
+    } catch (err) {
       next(err)
     }
   },
   postCategory: async (c: Context, next: (error?: any) => void) => {
     try {
-      const { name, icon } = await c.req.parseBody()
-      console.log('---', name, icon)
+      const { name, icon } = await c.req.json()
       if (!name) throw new Error('Name is required!')
       const newCategory = new Category({ name, icon })
-      newCategory.save()
+      await newCategory.save()
       return c.json(newCategory)
     } catch (err) {
-      console.log(err)
+      next(err)
+    }
+  },
+  putCategory: async (c: Context, next: (error?: any) => void) => {
+    try {
+      const { cid } = c.req.param()
+      const { name, icon } = await c.req.json()
+      if (!name) throw new Error('Name is required!')
+      const updatedCategory = await Category.findByIdAndUpdate(
+        cid,
+        { name, icon },
+        { new: true }
+      )
+      if (!updatedCategory) throw new Error('Category not found!')
+      return c.json(updatedCategory)
+    } catch (err) {
+      next(err)
+    }
+  },
+  deleteCategory: async (c: Context, next: (error?: any) => void) => {
+    try {
+      const { cid } = c.req.param()
+      const deletedCategory = await Category.findByIdAndDelete(cid)
+      if (!deletedCategory) throw new Error('Category not found!')
+      return c.json(deletedCategory)
+    } catch (err) {
       next(err)
     }
   },
--- src/routes/category.ts
+++ src/routes/category.ts
@@ -3,5 +3,8 @@ import { categoryController } from '../controllers/category'
 
 export function categoryRoute(app: Hono) {
   app.get('/api/category', categoryController.getCategories)
+  app.get('/api/category/:cid', categoryController.getCategory)
   app.post('/api/category', categoryController.postCategory)
+  app.put('/api/category/:cid', categoryController.putCategory)
+  app.delete('/api/category/:cid', categoryController.deleteCategory)
 }
