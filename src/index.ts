66a41bb6cd71346a12c9970f8ef502e32890f757
Tommy0901 <ak710180@gmail.com>
2024-05-12 12:30:23 +0800

    feat: add API error handler and enable CORS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
 import mongoose from 'mongoose'
 
 import { setupRoutes } from './routes'
@@ -9,12 +10,25 @@ const app = new Hono()
 mongoose.connect(process.env.MONGODB_URI!)
 
 app.use('*', logger())
+app.use(cors())
+
+setupRoutes(app) // 添加新的路由
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
-setupRoutes(app) // 添加新的路由
+;(app.onError as any)(async (err: Error, c: Context, next: () => void) => {
+  // 如果有錯誤發生，則處理錯誤；如果沒有錯誤，將控制權交給下一個中間件或路由
+  return err != null
+    ? err.message != null
+      ? c.json({
+          errors: [{ message: err.message }],
+        })
+      : c.json({
+          errors: [{ message: err }],
+        })
+    : next()
+})
 
 export default {
   port: process.env.PORT || 3000,
